acf277c5bf9dc54cc8826419e735f6bfb9e376f9
Liang <liang760206@gmail.com>
2018-08-22 10:04:28 +0800

    First practice

---
+++ pratice-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    <First/>
+  </div>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld'
+  import First from './components/First'
+
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld,
+      First
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ pratice-project/src/components/First.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="first">
+    <p>{{message}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "first",
+    data() {
+      return {
+        message: 'Tis is first component practice'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .first p {
+    color: red;
+    font-size: 30px;
+    font-weight: bold;
+  }
+</style>
